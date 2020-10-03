d164ee404da42e3fcc6761b7659a7c7d0ea0f29d
jhonDoe15 <62393042+jhonDoe15@users.noreply.github.com>
2020-10-03 15:26:35 +0300

    added semantic ui

--- react_front/src/App.js
+++ react_front/src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Card, Icon, /*Image*/ } from 'semantic-ui-react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card>
+        {/* <Image src='/images/avatar/large/matthew.png' wrapped ui={false} /> */}
+        <Card.Content>
+          <Card.Header>Matthew</Card.Header>
+          <Card.Meta>
+            <span className='date'>Joined in 2015</span>
+          </Card.Meta>
+          <Card.Description>
+            Matthew is a musician living in Nashville.
+            </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+            <Icon name='user' />
+              22 Friends
+        </Card.Content>
+      </Card>
     </div>
   );
 }
